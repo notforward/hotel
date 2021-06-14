86b00edf8c4aff79c032b424eab928469ff6851f
Artem <countryballsfa@gmail.com>
2021-06-14 10:15:42 +0300

    Custom tag added

---
+++ src/main/java/com/epam/project/hotel/tags/CommandTag.java
@@ -0,0 +1,20 @@
+package com.epam.project.hotel.tags;
+
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class CommandTag extends SimpleTagSupport {
+
+    private String command;
+
+    @Override
+    public void doTag() throws IOException {
+        JspWriter writer = getJspContext().getOut();
+        writer.write("<input type='hidden' name='command' value='" + command + "'/>");
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}
